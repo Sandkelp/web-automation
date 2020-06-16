21ef7962a882928cce9606c319631c3ef264c84d
Sandkelp <54632799+Sandkelp@users.noreply.github.com>
2020-06-16 10:10:44 -0400

    Update login.py

--- login.py
+++ login.py
@@ -1,3 +1,11 @@
-site = ''
-username = ''
-pw = '' 
+import london
+import pyautogui
+from time import sleep
+
+site = 'http://class-finder.com/'# the first site you want
+site2 = ''#the second site if you want it
+#london.open_guest_win((822,1055),(1849,64),(1554,466),'site')
+
+
+
+
